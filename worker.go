274d35e4a8a772fae4736db6bd9b6651241773d0
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2021-02-16 17:17:59 +0900

    add /stats/metrics/v2 endpoint

--- worker.go
+++ worker.go
@@ -1,28 +1,30 @@
 package sqsjkr
 
 import (
+	"sync/atomic"
+
 	"github.com/kayac/sqsjkr/throttle"
 )
 
 // Worker struct
 type Worker struct {
-	sjkr SQSJkr
-	id   int
-	jobs <-chan Job
-	busy chan struct{}
+	sjkr  SQSJkr
+	id    int
+	jobs  <-chan Job
+	stats *Stats
 }
 
 // SpawnWorker spawn worker
-func SpawnWorker(sjkr SQSJkr, wid int, js <-chan Job, busy chan struct{}) {
+func SpawnWorker(sjkr SQSJkr, wid int, js <-chan Job, s *Stats) {
 	defer func(id int) {
 		logger.Infof("[worker_id:%d] terminated command worker.", wid)
 	}(wid)
 
 	worker := Worker{
-		sjkr: sjkr,
-		id:   wid,
-		jobs: js,
-		busy: busy,
+		sjkr:  sjkr,
+		id:    wid,
+		jobs:  js,
+		stats: s,
 	}
 	logger.Infof("[worker_id:%d]spawn worker.", wid)
 
@@ -53,23 +55,27 @@ func (w Worker) ReceiveMessage() {
 
 func (w Worker) executeJob(job Job) error {
 	// busy worker number count up
-	w.busy <- struct{}{}
+	w.stats.busy <- struct{}{}
 
 	// decrement busy worker number when to return
 	defer func() {
-		<-w.busy
+		<-w.stats.busy
 	}()
 
 	// Execute job
 	logger.Infof("CMD event_id:%s command:%s", job.EventID(), job.Command())
 	output, err := job.Execute(w.sjkr.Locker())
 	if err != nil && output == nil {
+		atomic.AddInt64(&w.stats.Invocations.Failed, 1)
 		logger.Errorf("[event:%s] failed to invoke command, reason: %s", job.EventID(), err.Error())
 		return err
 	} else if err != nil {
+		atomic.AddInt64(&w.stats.Invocations.Errored, 1)
 		logger.Errorf("[event:%s] error when to invoke command, reason: %s", job.EventID(), err.Error())
 		logger.Errorf(string(output))
 		return err
+	} else {
+		atomic.AddInt64(&w.stats.Invocations.Succeeded, 1)
 	}
 	logger.Debugf("[event:%s] output:\n%s", job.EventID(), string(output))
 
